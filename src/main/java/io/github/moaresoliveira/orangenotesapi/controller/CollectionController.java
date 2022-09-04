d13ef614be770358b80ba919c59331cada8dc57c
MoaresOliveira <moaresoliveira@gmail.com>
2022-09-04 19:27:29 -0300

    Api Documentation

--- src/main/java/io/github/moaresoliveira/orangenotesapi/controller/CollectionController.java
+++ src/main/java/io/github/moaresoliveira/orangenotesapi/controller/CollectionController.java
@@ -5,10 +5,12 @@ import io.github.moaresoliveira.orangenotesapi.model.form.CollectionForm;
 import io.github.moaresoliveira.orangenotesapi.model.form.CollectionUpdateForm;
 import io.github.moaresoliveira.orangenotesapi.service.CollectionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Collection")
 @RestController
 @RequestMapping("/collections")
 public class CollectionController {
@@ -19,51 +21,51 @@ public class CollectionController {
         this.collectionService = collectionService;
     }
 
-    @Operation(description = "Get a list of Collection")
+    @Operation(summary = "Get a list of Collection")
     @GetMapping
     public List<CollectionDTO> getCollections() {
         return collectionService.findAll();
     }
 
-    @Operation(description = "Get a Collection by Id")
+    @Operation(summary = "Get a Collection by Id")
     @GetMapping("/{id}")
     public CollectionDTO getCollectionById(@PathVariable Long id) {
         return collectionService.findById(id);
     }
 
-    @Operation(description = "Get all content of a Collection by Id")
+    @Operation(summary = "Get all content of a Collection by Id")
     @GetMapping("/content/{id}")
     public List<CollectionDTO> getCollectionContent(@PathVariable Long id) {
         return collectionService.findCollectionContent(id);
     }
 
-    @Operation(description = "Create a new Collection")
+    @Operation(summary = "Create a new Collection")
     @PostMapping
     public CollectionDTO createCollection(@RequestBody CollectionForm collection) {
         System.out.println(collection);
         return collectionService.save(collection);
     }
 
-    @Operation(description = "Update a Collection by Id")
+    @Operation(summary = "Update a Collection by Id")
     @PutMapping("/{id}")
     public CollectionDTO updateCollection(@PathVariable Long id,@RequestBody CollectionUpdateForm collection) {
         return collectionService.update(id,collection);
     }
 
-    @Operation(description = "Set a Collection/Content as finished")
+    @Operation(summary = "Set a Collection/Content as finished")
     @PatchMapping("/{id}")
     public CollectionDTO finishCollection(@PathVariable Long id) {
         return collectionService.finishCollection(id);
     }
 
-    @Operation(description = "Add Content to a Collection")
+    @Operation(summary = "Add Content to a Collection")
     @PostMapping("/add/{id}")
     public CollectionDTO addContentToCollection(@PathVariable Long id,@RequestBody CollectionForm content) {
         System.out.println("Controller: collection a adicionar => " + content);
         return collectionService.addContentToCollection(id, content);
     }
 
-    @Operation(description = "Delete a Collection")
+    @Operation(summary = "Delete a Collection")
     @DeleteMapping("/{id}")
     public boolean deleteCollection(@PathVariable Long id) {
         return collectionService.deleteById(id);
